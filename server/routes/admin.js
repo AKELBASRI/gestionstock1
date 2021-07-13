19f2d3f941ec3c4c1ee5ed0ae474b0da0e6c19b0
AHMED KHALIL EL BASRI <ahmedkhalilelbasri@gmail.com>
2021-07-13 11:33:23 +0100

    update password

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const {getOneUser,createUser, getusers, updatepassword, updateUser, deleteUser } = require("../controllers/adminController");
-const { requireSignIn, isAuth, isAdmin } = require("../middleware/auth");
+const { requireSignIn, isAuth } = require("../middleware/auth");
 const { userById } = require("../middleware/admin");
 const router = express.Router();
 
  router.get("/alladmins/:mle",requireSignIn,isAuth,getusers);
+ router.put("/updatepassword/:mle",requireSignIn,isAuth,updatepassword)
 router.param("mle", userById);
 
 module.exports = router;
