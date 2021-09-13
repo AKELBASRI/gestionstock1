5ec43b815c72e1c931c4bb54d0dd578cc841d1b9
Mac <>
2021-09-13 22:47:30 +0100

    sort list of react select

--- client/src/core/Components/ReactHookReactSelect.js
+++ client/src/core/Components/ReactHookReactSelect.js
@@ -3,6 +3,9 @@ import Select from "react-select";
 import { Controller } from "react-hook-form";
 const ReactHookFormReactSelect = (Props) => {
   const { options, control, Name, reef, Value, onchange, ...props } = Props;
+  function compare(a, b) {
+    return a.label > b.label ? 1 : b.label > a.label ? -1 : 0;
+  }
 
   return (
     <FormControl {...props}>
@@ -12,8 +15,9 @@ const ReactHookFormReactSelect = (Props) => {
         name={Name}
         render={({ field: { onChange, value } }) => (
           <Select
+            ignoreAccents={true}
             inputRef={reef?.ref}
-            options={options}
+            options={options.sort(compare)}
             value={options?.find((c) => c.value === value) || Value}
             onChange={(val) => {
               onChange(val?.value);
