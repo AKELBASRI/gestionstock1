d714d19aee3801e29c9b477ceadf321c00b93526
AKELBASRI <57672990+AKELBASRI@users.noreply.github.com>
2021-09-17 16:23:37 +0100

    liste materiels add expanded rows

--- client/src/components/pages/admin/materiels/ListeMateriels.jsx
+++ client/src/components/pages/admin/materiels/ListeMateriels.jsx
@@ -3,13 +3,33 @@ import { useDispatch, useSelector } from "react-redux";
 import Layout from "../../Layout/Layout";
 import MUIDataTable from "mui-datatables";
 import Switch from "@material-ui/core/Switch";
-
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
 import AddEditSaisieMaterielModal from "./AddEditSaisieMaterielModal";
 import handleClickDelete from "./DeleteMateriel";
 import AffecterMaterielModal from "./AffecterMaterielModal";
 import { flattenObject } from "../../../../core/ApiCore";
 import { Delete } from "../../../../core/util";
-import { Box, makeStyles } from "@material-ui/core";
+import Paper from "@material-ui/core/Paper";
+import { withStyles } from "@material-ui/core/styles";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+const StyledPaper = withStyles(() => ({
+  root: {
+    padding: 8,
+    // border: "1px solid black",
+    marginLeft: "60px",
+    boxShadow: "none",
+  },
+}))(Paper);
+
+import {
+  Box,
+  makeStyles,
+  Table,
+  TableBody,
+  TableContainer,
+  TableHead,
+} from "@material-ui/core";
 import { ColorButton } from "../../../../core/styleModalForm";
 import CreateIcon from "@material-ui/icons/Create";
 import LinkIcon from "@material-ui/icons/Link";
@@ -19,6 +39,7 @@ import {
   FetchTotalAvailableMateriels,
   FetchTotalMateriels,
 } from "../../../../store/actions";
+import { createTheme } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -28,6 +49,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 function ListMateriels() {
+  const [rowsExpanded] = useState();
   const classes = useStyles();
   const dispatch = useDispatch();
   const [materiel, setMateriel] = useState({});
@@ -131,6 +153,7 @@ function ListMateriels() {
       label: "Garentie",
       name: "garentie",
       options: {
+        display: false,
         filter: true,
         sort: false,
         print: false,
@@ -153,6 +176,7 @@ function ListMateriels() {
       options: {
         filter: true,
         print: false,
+        display: false,
       },
     },
     {
@@ -276,6 +300,7 @@ function ListMateriels() {
       label: "Agent",
       name: "agent.agent_full_name",
       options: {
+        display: false,
         filter: true,
       },
     },
@@ -290,6 +315,7 @@ function ListMateriels() {
       label: "Fournisseur",
       name: "fournisseur.NomFournisseur",
       options: {
+        display: false,
         print: false,
         filter: true,
       },
@@ -304,9 +330,11 @@ function ListMateriels() {
 
     {
       label: "Lieu",
+
       name: "lieu.lieu",
       options: {
         filter: true,
+        display: false,
       },
     },
 
@@ -324,10 +352,148 @@ function ListMateriels() {
       },
     },
   ];
+  const theme = createTheme({
+    overrides: {
+      MUIDataTableSelectCell: {
+        expandDisabled: {
+          // Soft hide the button.
+          visibility: "hidden",
+        },
+      },
+    },
+  });
   const options = {
     pagination: true,
     filter: true,
     filterType: "multiselect",
+    expandableRows: true,
+    expandableRowsHeader: false,
+    expandableRowsOnClick: true,
+    rowsExpanded: rowsExpanded,
+    isRowExpandable: (dataIndex) => {
+      const garentie = listmateriels[dataIndex][3];
+      const agent = listmateriels[dataIndex][16];
+      const fournisseur = listmateriels[dataIndex][18];
+      const Lieu = listmateriels[dataIndex][20];
+      const datereceptionprov = listmateriels[dataIndex][4];
+      const agence = listmateriels[dataIndex][15];
+      if (
+        agent === null &&
+        agence === null &&
+        fournisseur === null &&
+        Lieu === null &&
+        datereceptionprov === null &&
+        garentie === null
+      ) {
+        return false;
+      }
+      return true;
+      // Prevent expand/collapse of any row if there are 4 rows expanded already (but allow those already expanded to be collapsed)
+    },
+    onRowExpansionChange: (_, allRowsExpanded) => {
+      // setRowsExpanded(allRowsExpanded.slice(-1).map((item) => item.index));
+      console.log(allRowsExpanded);
+    },
+    // onTableChange: (action, tableState) => {
+    //   console.log(action);
+    //   switch (action) {
+    //     case "rowExpansionChange":
+    //       console.log(action);
+    //       console.dir(tableState);
+    //       var rowsExpanded = tableState.expandedRows.data.map(
+    //         (item) => item.index
+    //       );
+
+    //       if (rowsExpanded.length > 1) {
+    //         // limiting would go here
+    //         rowsExpanded = rowsExpanded.slice(-1);
+    //       }
+
+    //       console.dir(rowsExpanded);
+
+    //       setRowsExpanded(rowsExpanded);
+
+    //       break;
+    //   }
+    // },
+    renderExpandableRow: function displayexpanded(rowData, rowMeta) {
+      // const colSpan = rowData.length + 1;
+
+      const garentie = listmateriels[rowMeta.dataIndex][3];
+      const agent = listmateriels[rowMeta.dataIndex][16];
+      const fournisseur = listmateriels[rowMeta.dataIndex][18];
+      const Lieu = listmateriels[rowMeta.dataIndex][20];
+      const datereceptionprov = listmateriels[rowMeta.dataIndex][4];
+      const agence = listmateriels[rowMeta.dataIndex][15];
+      // const colSpan = rowData.length;
+      return (
+        // <React.Fragment>
+        <tr>
+          <td colSpan={7}>
+            <TableContainer component={StyledPaper}>
+              <Table style={{ minWidth: "650" }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    {garentie && <TableCell>Garentie</TableCell>}
+                    {agent && <TableCell align="left">Agent</TableCell>}
+                    {fournisseur && (
+                      <TableCell align="left">Fournisseur</TableCell>
+                    )}
+                    {Lieu && <TableCell align="left">Lieu</TableCell>}
+
+                    {datereceptionprov && (
+                      <TableCell align="left">
+                        Date reception provisoire
+                      </TableCell>
+                    )}
+                    {agence && <TableCell align="left">Agence</TableCell>}
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  <TableRow>
+                    {garentie && (
+                      <TableCell component="th" scope="row">
+                        {parseInt(garentie) === 1
+                          ? garentie + " an"
+                          : garentie && parseInt(garentie) === 2
+                          ? garentie + " ans"
+                          : null}
+                      </TableCell>
+                    )}
+
+                    {agent && (
+                      <TableCell component="th" scope="row">
+                        {agent}
+                      </TableCell>
+                    )}
+                    {fournisseur && (
+                      <TableCell component="th" scope="row">
+                        {fournisseur}
+                      </TableCell>
+                    )}
+                    {Lieu && (
+                      <TableCell component="th" scope="row">
+                        {Lieu}
+                      </TableCell>
+                    )}
+                    {datereceptionprov && (
+                      <TableCell component="th" scope="row">
+                        {datereceptionprov}
+                      </TableCell>
+                    )}
+                    {agence && (
+                      <TableCell component="th" scope="row">
+                        {agence}
+                      </TableCell>
+                    )}
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </td>
+        </tr>
+      );
+    },
     // filter: true,
     // filterType: 'dropdown',
     responsive: "standard",
@@ -342,12 +508,14 @@ function ListMateriels() {
         nouveau Materiel
       </ColorButton>
       {listmateriels && (
-        <MUIDataTable
-          title={"Liste des Materiels"}
-          data={listmateriels}
-          columns={columns}
-          options={options}
-        />
+        <MuiThemeProvider theme={theme}>
+          <MUIDataTable
+            title={"Liste des Materiels"}
+            data={listmateriels}
+            columns={columns}
+            options={options}
+          />
+        </MuiThemeProvider>
       )}
       <AffecterMaterielModal
         codemtrl={materiel.idmateriel}
