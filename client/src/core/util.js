cd726d50e26522406dac1ab6dc3cbf345187eac5
Mac <>
2021-09-17 21:53:33 +0100

    add DeleteMessagebox for delete

--- client/src/core/util.js
+++ client/src/core/util.js
@@ -22,7 +22,39 @@ export const Delete = (user, action, handleClickDelete) => {
     },
   });
 };
-
+export const DeleteMessageBox = () => {
+  return new Promise(function (resolve) {
+    confirmAlert({
+      closeOnClickOutside: false,
+      customUI: function showdialog({ onClose }) {
+        return (
+          <div className="custom-ui">
+            <h1>Vous êtes sure ?</h1>
+            <p>
+              Voulez-vous Vraiment supprimer le(s) element(s) selectionné(s) ?
+            </p>
+            <button
+              onClick={() => {
+                resolve(false);
+                onClose();
+              }}
+            >
+              Non
+            </button>
+            <button
+              onClick={() => {
+                resolve(true);
+                onClose();
+              }}
+            >
+              Oui, Supprimer !
+            </button>
+          </div>
+        );
+      },
+    });
+  });
+};
 export const RequestError = () => (
   <p>There was some error during fetching. Please try again.</p>
 );
